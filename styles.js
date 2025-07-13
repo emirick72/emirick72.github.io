0b4a0fc705306bb64ce167dcae127ffaee68e880
Emily Rick <emrick.2261@gmail.com>
2025-07-13 00:13:56 -0400

    started implementing the side navbar and added a javascript file to the project

---
+++ styles.js
@@ -0,0 +1,9 @@
+/*Set the width of the side navbar to 250px*/
+function openNav() {
+    document.getElementById("mySidenavbar").style.width = "250px";
+}
+
+/*Set the width of the side navbar to 0*/
+function closeNav() {
+    document.getElementById("mySidenavbar").style.width = "0";
+}
